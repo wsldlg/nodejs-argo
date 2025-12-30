bdd43d1945247106f72471962466a69dd8859c96
wsldlg <wsldlg@gmail.com>
2025-12-30 20:01:23 +0800

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const axios = require("axios");
@@ -11,19 +10,19 @@ const { execSync } = require('child_process');        // 只填写UPLOAD_URL将�
 const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动上传地址,需填写部署Merge-sub项目后的首页地址,例如：https://merge.xxx.com
 const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
-const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
-const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
+const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录  
+const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径  
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
 const UUID = process.env.UUID || '877aae23-e2e6-48c6-aa29-854ee6af70f6'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
-const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com      
+const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写  
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'railway.glls.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGJjNzFkMmEwZTBlZDE0YTBmYzI5NzViNTU0YWJkYTkiLCJ0IjoiY2FlODU4ZmItODg4MS00NzA5LTlhZTktYzEwYmI1YTg0NDliIiwicyI6Ik1UUmxZell3Wm1JdE1EWm1aaTAwWkRjeExUZ3hZemd0WVRJM05XTXdNbVl6WW1GbCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGJjNzFkMmEwZTBlZDE0YTBmYzI5NzViNTU0YWJkYTkiLCJ0IjoiY2FlODU4ZmItODg4MS00NzA5LTlhZTktYzEwYmI1YTg0NDliIiwicyI6Ik1UUmxZell3Wm1JdE1EWm1aaTAwWkRjeExUZ3hZemd0WVRJM05XTXdNbVl6WW1GbCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id  
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'railway';                        // 节点名称
+const NAME = process.env.NAME || '';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
@@ -605,7 +604,7 @@ async function AddVisitTask() {
 async function startserver() {
   try {
     argoType();
-    deleteNodes();
+    deleteNodes();  
     cleanupOldFiles();
     await generateConfig();
     await downloadFilesAndRun();
